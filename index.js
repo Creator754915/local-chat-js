dcf1e06c7a84ae043794dabadb0ce3608cf915e6
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2022-09-02 18:16:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+io.on('connection', (socket) => {
+    console.log('Un utilisateur s\'est connecté')
+    
+    socket.on('disconnect', () => {
+        console.log('Un utilisateur c\'est déconecté')
+    })
+
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    });
+});
+
+
+
+
+server.listen(5000, () => {
+    console.log('Ecout sur le port 5000')
+});
